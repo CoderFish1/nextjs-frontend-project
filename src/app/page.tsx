4e49f3fdff6b86d506d0df697f96954445649d17
Shrey Pandey <shreypandey22005@gmail.com>
2025-10-04 02:46:11 +0530

    sticky scroll reveal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
+import { WhyChoose } from "@/components/WhyChoose";
 
 export default function Home() {
   return (
     <>
     <HeroSection/>
     <FeaturedCourses/>
+    <WhyChoose/>
     </>
   );
 }
--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -4,6 +4,8 @@ import courseData from "../data/music_courses.json";
 import Link from "next/link";
 import { Button } from "./ui/moving-border";
 import { BackgroundGradient } from "./ui/background-gradient";
+import { HoverBorderGradient } from "./ui/hover-border-gradient";
+
 
 // creating our own type of  json in typescript
 interface Course {
@@ -23,40 +25,39 @@ const FeaturedCourses = () => {
 
   return (
     <div className="w-full bg-gray-800 relative h-auto">
-      <div className="text-5xl font-bold text-center pt-24 pb-16">
+      <div className=" text-2xl md:text-5xl font-bold text-center pt-24 pb-16">
         Learn with the Best
       </div>
-     
+
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8 px-6 pb-16">
         {featuredData.map((course: Course) => (
-           <BackgroundGradient>
-           <div
-            key={course.id}
-            className="p-6 rounded-lg flex flex-col justify-between"
-          >
-            <div>
-             
-              <div className="text-2xl font-semibold text-center mb-4">
-                {course.title}
+          <BackgroundGradient key={course.id}>
+            <div className="p-6 rounded-lg flex flex-col justify-between">
+              <div>
+                <div className="text-2xl font-semibold text-center mb-4">
+                  {course.title}
+                </div>
+                <p className="text-base text-center mb-6">
+                  {course.description}
+                </p>
               </div>
-              <p className="text-base text-center mb-6">
-                {course.description}
-              </p>
+              <div className="flex justify-center">
+                <Link href={`/courses/${course.slug}`}>
+                  <Button
+                    borderRadius="1.75rem"
+                    className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+                  >
+                    LEARN MORE
+                  </Button>
+                </Link>
+              </div>
             </div>
-            <div className="flex justify-center">
-              <Link href={`/courses/${course.slug}`}>
-                <Button
-                  borderRadius="1.75rem"
-                  className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
-                >
-                  LEARN MORE
-                </Button>
-              </Link>
-            </div>
-          </div>
           </BackgroundGradient>
         ))}
       </div>
+      <Link href={"/courses"}>
+      <div className="flex justify-center p-5 "> <HoverBorderGradient>View Courses</HoverBorderGradient> </div>
+      </Link>
     </div>
   );
 };
