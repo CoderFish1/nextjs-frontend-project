d1993e574394fbd428a4c173d5ee0dcd6ddb15dd
Shrey Pandey <shreypandey22005@gmail.com>
2025-10-03 21:51:46 +0530

    nav and herosection added

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { cn } from "@/utils/lib/util";
+import { Spotlight } from "./ui/Spotlight";
+import { HoverBorderGradient } from "./ui/hover-border-gradient";
+
+const HeroSection = () => {
+  return (
+    <div className="h-100vw md:h-[40rem] w-full flex flex-col items-center justify-center relative mx-auto md:py-0 py-10 overflow-hidden">
+      <Spotlight className="-top-0 left-0 md:-top-20 md:left-60" fill="white" />
+      <div className="w-full text-center relative z-10 p-4"></div>
+      <h1 className=" text-3xl mt-20 text-nowrap md:text-6xl md:p-0 font-bold bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-transparent">
+        Master the Art of Music
+      </h1>
+
+      <p className="text-center mx-auto w-[70%] mt-5 md:text-xl ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio magni
+        assumenda quo fuga velit odio provident quisquam quia. Exercitationem
+        iure consequatur non placeat.
+      </p>
+      <div className="mt-6">
+        <Link href={"/courses"}>
+          <HoverBorderGradient
+            containerClassName="rounded-full"
+            as="button"
+            className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2"
+          >
+            Explore Courses
+          </HoverBorderGradient>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
